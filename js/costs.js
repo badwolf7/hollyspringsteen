d39fc143e4f012f80ddc7b1bbcccab3650a390b8
Holly Springsteen <hhspringsteen@gmail.com>
2015-03-26 23:24:16 -0400

    add prices to charge clients

---
+++ js/costs.js
@@ -0,0 +1,115 @@
+// Holly Springsteen
+// Prices - 2015
+
+{
+	pages: {
+		1-10: {
+			low: 1000,
+			high: 2000
+		},
+		10-50: {
+			low: 2000,
+			high: 3000
+		},
+		50-150: {
+			low: 3000,
+			high: 6500
+		},
+		150-250: {
+			low: 6500,
+			high: 10000
+		}
+	},
+	design: {
+		simple: {
+			low: 2000,
+			high: 3000
+		},
+		moderate: {
+			low: 3000,
+			high: 5000
+		},
+		"high end": {
+			low: 5000,
+			high: 10000
+		},
+		"world class": {
+			low: 10000,
+			high: 15000
+		}
+	},
+	copywriting: {
+		5-10: {
+			low: 1000,
+			high: 1500
+		},
+		10-25: {
+			low: 1500,
+			high: 3750
+		},
+		25-50: {
+			low: 3750,
+			high: 7500
+		}
+	},
+	seo: {
+		30: {
+			low: 2000,
+			high: 4000
+		},
+		80: {
+			low: 4000,
+			high: 6000
+		},
+		150: {
+			low: 6000,
+			high: 10000
+		}
+	},
+	responsive: {
+		no: 0,
+		yes: 3000
+	},
+	database: {
+		basic: {
+			low: 2000,
+			high: 4000
+		},
+		advanced: {
+			low: 4000,
+			high: 10000
+		},
+		enterprise: {
+			low: 10000,
+			high: 25000
+		}
+	},
+	e-commerce: {
+		basic: {
+			low: 2000,
+			high: 4000
+		},
+		advanced: {
+			low: 4000,
+			high: 10000
+		},
+		enterprise: {
+			low: 10000,
+			high: 25000
+		}
+	},
+	cms: {
+		basic: {
+			low: 2000,
+			high: 4000
+		},
+		advanced: {
+			low: 4000,
+			high: 10000
+		},
+		enterprise: {
+			low: 10000,
+			high: 25000
+		}
+	}
+}
